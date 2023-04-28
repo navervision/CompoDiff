ebe86d640398e64db021c0d5afda097dda5321a0
geonm <korgm403@gmail.com>
2023-04-28 17:22:03 +0900

    version up

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='compodiff',
-    version='0.1.0',
+    version='0.1.1',
     description='Easy to use CompoDiff library',
     author='NAVER Corp.',
     author_email='dl_visionresearch@navercorp.com',
